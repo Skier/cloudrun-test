6cf6fddf62d46be427f6affe1a1b7ee7d81b838a
Valery Yaworsky <yaworsky@gmail.com>
2025-12-05 23:56:20 -0500

    Test Cloud Build trigger Fixing logs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from Cloud Run via Cloud Build! Fixed Logger. ");
+app.MapGet("/", () => "Hello from Cloud Run via Cloud Build! Fixing Logger. ");
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
