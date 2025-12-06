79a7af6bdd9a983aaf936ffe279837db181e5573
Valery Yaworsky <yaworsky@gmail.com>
2025-12-05 23:28:09 -0500

    Test Cloud Build trigger

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext ctx) =>
-{
-    Console.WriteLine($"Request from {ctx.Connection.RemoteIpAddress}");
-    return "Hello from Cloud Run!";
-});
+app.MapGet("/", () => "Hello from Cloud Run via Cloud Build!");
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
